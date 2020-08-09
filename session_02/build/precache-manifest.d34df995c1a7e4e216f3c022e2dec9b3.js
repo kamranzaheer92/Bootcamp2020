5c48c1b38eb66eeb25db00367fa5b47bb8217ae1
kamranzaheer92 <68019731+kamranzaheer92@users.noreply.github.com>
2020-08-09 18:54:31 +0500

    delete session_02 folder

---
+++ session_02/build/precache-manifest.d34df995c1a7e4e216f3c022e2dec9b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6956e263f615059e7dc74916cd8a610a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "104ee459df5e7f570ef2",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d844ab1bdce0a9863587",
+    "url": "/static/js/2.ca679e3a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ca679e3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "104ee459df5e7f570ef2",
+    "url": "/static/js/main.51848900.chunk.js"
+  },
+  {
+    "revision": "e1e8d193ede6fb65437c",
+    "url": "/static/js/runtime-main.9f415ead.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
